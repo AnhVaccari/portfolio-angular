b3e0a0c88ca586670bc7f1f9858b5ddf7e102e50
AnhVaccari <anh.vaccari@orange.fr>
2026-01-12 10:52:47 +0100

    feat: add photos home and about pages

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,10 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  isLoading = false;
+  showSuccessMessage = false;
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subject: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.isLoading = true;
+
+      // Simulation d'envoi (2 secondes)
+      setTimeout(() => {
+        this.isLoading = false;
+        this.showSuccessMessage = true;
+        this.contactForm.reset();
+
+        // Cache le message après 3 secondes
+        setTimeout(() => {
+          this.showSuccessMessage = false;
+        }, 3000);
+      }, 2000);
+    }
+  }
+}
